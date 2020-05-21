f0c3f9d3bacfe6480a22ca5df0f483004ae6f115
mark <markpackham1@gmail.com>
2020-05-21 16:23:38 +0100

    Add generic settings for hello world

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,24 @@
 <template>
-  
+  <div>
+    <main-header></main-header>
+    <main-footer></main-footer>
+  </div>
 </template>
 
 <script>
+import MainHeader from "./components/layouts/MainHeader";
+import MainFooter from "./components/layouts/MainFooter";
 export default {
-
-}
+  name: "app",
+  data() {},
+  methods: {},
+  mounted() {},
+  components: {
+    "main-header": MainHeader,
+    "main-footer": MainFooter
+  }
+};
 </script>
 
 <style>
-
 </style>
---
+++ client/src/components/layouts/MainFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>Copyright 2020</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
