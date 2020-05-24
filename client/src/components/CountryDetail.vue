55405b04b79cb8a5262a8c4554e8b8fed9869985
Martin Graves 422 <33223356+markpackham@users.noreply.github.com>
2020-05-24 19:34:35 +0100

    Create modal

--- client/src/components/CountryDetail.vue
+++ client/src/components/CountryDetail.vue
@@ -1,26 +1,58 @@
 <template>
   <div class="country-detail" v-if="country">
     <h3>{{country.name}}</h3>
+    <img class="flag" name="flag" :src="country.flag" />
     <p>Alt Name: {{country.nativeName}}</p>
     <p>
       Sub Region:
       <strong>{{country.subregion}}</strong>
     </p>
-    <p>
-      Pop: {{country.population}}
-      <br />
-      Area: {{country.area}} km²
-      <br />
-      Pop Density: {{ (country.population / country.area).toFixed(2) }}
-    </p>
+    <p>Pop Density: {{ (country.population / country.area).toFixed(2) }}</p>
     <p>Capital: {{country.capital}}</p>
-    <img class="flag" name="flag" :src="country.flag" />
-    <p>
-      <a
-        target="_blank"
-        :href="`https://www.cia.gov/library/publications/the-world-factbook/geos/${country.alpha2Code.toLowerCase()}.html`"
-      >More info &#128366;</a>
-    </p>
+    <div class="modal-container">
+      <input type="checkbox" id="model-click" style="display:none" />
+      <label for="model-click">
+        <a class="button-theme">Full info</a>
+      </label>
+      <div class="country_modal">
+        <div class="modal_background">
+          <img class="flag" name="flag" :src="country.flag" />
+          <h3>
+            {{country.name}}
+            <label for="model-click" class="btn-close">
+              <a class="button-theme">
+                <strong>Close &#10006;</strong>
+              </a>
+            </label>
+          </h3>
+          <ul>
+            <li>Capital: {{country.capital}}</li>
+            <li>Region: {{country.region}}</li>
+            <li>Sub Region: {{country.subregion}}</li>
+            <li>Population: {{country.population}}</li>
+            <li>Area: {{country.area}}</li>
+            <li>Pop Density: {{ (country.population / country.area).toFixed(2) }}</li>
+            <li>Demonym: {{country.demonym}}</li>
+            <li>Gini Coefficient: {{country.gini}}</li>
+            <li>Native Name: {{country.nativeName}}</li>
+            <li>Languages:</li>
+            <ul v-for="(ctry, index) in country.languages" :key="index">
+              <li>&#128172; {{ctry.name}}</li>
+            </ul>
+            <li>Currencies:</li>
+            <ul v-for="(ctry, index) in country.currencies" :key="index">
+              <li>&#128176; {{ctry.name}}</li>
+            </ul>
+          </ul>
+        </div>
+      </div>
+      <p>
+        <a
+          target="_blank"
+          :href="`https://www.cia.gov/library/publications/the-world-factbook/geos/${country.alpha2Code.toLowerCase()}.html`"
+        >More info &#128366;</a>
+      </p>
+    </div>
   </div>
 </template>
 
@@ -32,4 +64,5 @@ export default {
 </script>
 
 <style>
+@import "../assets/styles/modal.css";
 </style>
