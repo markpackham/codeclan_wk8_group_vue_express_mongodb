8f6ac0ac41ad7dcd771f1a23d806ec09e646f34a
mark <markpackham1@gmail.com>
2020-05-24 13:20:52 +0100

    Update Country Details with external link for more info

--- client/src/components/CountryDetail.vue
+++ client/src/components/CountryDetail.vue
@@ -2,11 +2,25 @@
   <div class="country-detail" v-if="country">
     <h3>{{country.name}}</h3>
     <p>Alt Name: {{country.nativeName}}</p>
-    <p>Sub Region: <strong>{{country.subregion}}</strong></p>
-    <p>Pop: {{country.population}} <br> Area: {{country.area}} km²
-    <br> Pop Density: {{ (country.population / country.area).toFixed(2) }}</p>
+    <p>
+      Sub Region:
+      <strong>{{country.subregion}}</strong>
+    </p>
+    <p>
+      Pop: {{country.population}}
+      <br />
+      Area: {{country.area}} km²
+      <br />
+      Pop Density: {{ (country.population / country.area).toFixed(2) }}
+    </p>
     <p>Capital: {{country.capital}}</p>
     <img class="flag" name="flag" :src="country.flag" />
+    <p>
+      <a
+        target="_blank"
+        :href="`https://www.cia.gov/library/publications/the-world-factbook/geos/${country.alpha2Code.toLowerCase()}.html`"
+      >More info</a>
+    </p>
   </div>
 </template>
 
