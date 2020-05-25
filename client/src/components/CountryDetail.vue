0118bbc726205828a7462c38e1a8e9f43bdd24c5
mark <markpackham1@gmail.com>
2020-05-25 18:47:12 +0100

    Fix url for map lat and longitude
    Solved with :src="
              `http://www.openlinkmap.org/small.php?lat=${country.latlng[0]}&lon=${country.latlng[1]}&zoom=5`
            "

--- client/src/components/CountryDetail.vue
+++ client/src/components/CountryDetail.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="country-detail" v-if="country">
-    <h3>{{country.name}} &#x1F310;</h3>
+    <h3>{{ country.name }} &#x1F310;</h3>
     <img class="flag" name="flag" :src="country.flag" />
-    <p>Alt Name: {{country.nativeName}}</p>
+    <p>Alt Name: {{ country.nativeName }}</p>
     <p>
       Sub Region:
-      <strong>{{country.subregion}}</strong>
+      <strong>{{ country.subregion }}</strong>
     </p>
     <p>Pop Density: {{ (country.population / country.area).toFixed(2) }}</p>
-    <p>Capital: {{country.capital}}</p>
+    <p>Capital: {{ country.capital }}</p>
     <div class="modal-container">
       <input type="checkbox" id="model-click" style="display:none" />
       <label for="model-click">
@@ -18,7 +18,7 @@
         <div class="modal_background">
           <img class="flag" name="flag" :src="country.flag" />
           <h3>
-            {{country.name}} &#127760;
+            {{ country.name }} &#127760;
             <label for="model-click" class="btn-close">
               <span class="button-theme">
                 <strong>Close &#10006;</strong>
@@ -26,23 +26,28 @@
             </label>
           </h3>
           <ul>
-            <li>Alternative Name: {{country.nativeName}}</li>
-            <li>Capital: {{country.capital}}</li>
-            <li>Region: {{country.region}}</li>
-            <li>Sub Region: {{country.subregion}}</li>
-            <li>Population: {{country.population}}</li>
-            <li>Area: {{country.area}}</li>
-            <li>Pop Density: {{ (country.population / country.area).toFixed(2) }}</li>
-            <li>Demonym: {{country.demonym}}</li>
-            <li>Gini Coefficient: {{country.gini}}</li>
-            <li>Native Name: {{country.nativeName}}</li>
+            <li>Alternative Name: {{ country.nativeName }}</li>
+            <li>Capital: {{ country.capital }}</li>
+            <li>Region: {{ country.region }}</li>
+            <li>Sub Region: {{ country.subregion }}</li>
+            <li>Population: {{ country.population }}</li>
+            <li>Area: {{ country.area }}</li>
+            <li>
+              Pop Density: {{ (country.population / country.area).toFixed(2) }}
+            </li>
+            <li>Demonym: {{ country.demonym }}</li>
+            <li>Gini Coefficient: {{ country.gini }}</li>
+            <li>Native Name: {{ country.nativeName }}</li>
             <li>Languages:</li>
             <ul v-for="(ctry, index) in country.languages" :key="index">
-              <li>&#128172; {{ctry.name}}</li>
+              <li>&#128172; {{ ctry.name }}</li>
             </ul>
             <li>Currencies:</li>
-            <ul v-for="(cty, index) in country.currencies" :key="(index + country.length)">
-              <li>&#128176; {{cty.name}}</li>
+            <ul
+              v-for="(cty, index) in country.currencies"
+              :key="index + country.length"
+            >
+              <li>&#128176; {{ cty.name }}</li>
             </ul>
           </ul>
         </div>
@@ -50,10 +55,24 @@
       <p>
         <a
           target="_blank"
-          :href="`https://www.cia.gov/library/publications/the-world-factbook/geos/${country.alpha2Code.toLowerCase()}.html`"
-        >CIA Factbook &#x1F4DA;</a>
+          :href="
+            `https://www.cia.gov/library/publications/the-world-factbook/geos/${country.alpha2Code.toLowerCase()}.html`
+          "
+          >CIA Factbook &#x1F4DA;</a
+        >
       </p>
-      <iframe width="420" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.openlinkmap.org/small.php?lat=<LAT>&lon=<LON>&zoom=<ZOOM>" style="border: 1px solid black"></iframe>
+      <iframe
+        width="420"
+        height="350"
+        frameborder="0"
+        scrolling="no"
+        marginheight="0"
+        marginwidth="0"
+        :src="
+          `http://www.openlinkmap.org/small.php?lat=${country.latlng[0]}&lon=${country.latlng[1]}&zoom=5`
+        "
+        style="border: 1px solid black"
+      ></iframe>
     </div>
   </div>
 </template>
@@ -61,13 +80,10 @@
 <script>
 export default {
   name: "country-detail",
-  props: ["country"]
+  props: ["country"],
 };
 </script>
 
 <style>
 @import "../assets/styles/modal.css";
 </style>
-
-
-
