1e182380dd0b051833ec298801974e1c7a9e1065
Nathan <nathankirley@gmail.com>
2020-05-25 15:17:38 +0100

    working on selecting correct answer + removed question detail

--- client/src/components/QuestionHolder.vue
+++ client/src/components/QuestionHolder.vue
@@ -3,11 +3,12 @@
       <ul>
       <question-detail v-for="(question, index) in questions" :key="index"></question-detail>
       </ul>
-      <button v-on:click="handleClick">Next question</button>
+      <button v-on:click="nextQuestion">Next question</button>
       <div v-if="aQuestion">
       <p>{{aQuestion}}</p>
       <ul v-for="(qstn, index) in allAnswers" :key="index">
               <li><button>{{qstn}}</button></li>
+              <!-- v-if="this.answer" :class="this.answerStyle" -->
         </ul>
       </div>
   </div>
@@ -15,7 +16,6 @@
 
 <script>
 import { eventBus } from '@/main.js';
-import QuestionDetail from './QuestionDetail.vue';
 
 export default {
     name: 'question-holder',
@@ -23,17 +23,22 @@ export default {
     data(){
         return {
             aQuestion: "",
-            allAnswers: []
+            correctAnswer: '',
+            allAnswers: [],
+            // answer: ''
         }
     },
-    components: {
-      "question-detail": QuestionDetail
+    computed: {
+        answerStyle: function() {
+            if (this.answer == this.correctAnswer) {return "highlight-correct"}
+            else {return "highlight-incorrect"}
+        }
     },
     methods: {
-        handleClick() {
+        nextQuestion() {
             let randomNum = Math.floor(Math.random() * this.questions.length + 1);
             this.aQuestion = this.questions[randomNum].question;
-            // this.correctAnswer = this.questions[randomNum].correct_answer;
+            this.correctAnswer = this.questions[randomNum].correct_answer;
             this.allAnswers.splice(0, 1, this.questions[randomNum].correct_answer)
             this.allAnswers.splice(1, 1, this.questions[randomNum].incorrect_answers[0])
             this.allAnswers.splice(2, 1, this.questions[randomNum].incorrect_answers[1])
@@ -45,5 +50,12 @@ export default {
 </script>
 
 <style>
-
+.highlight-correct{
+    color: #48FF1B;
+    border: 3px solid #48FF1B;
+}
+.highlight-incorrect{
+    color: #FF2525;
+    border: 3px solid #FF2525;
+}
 </style>
