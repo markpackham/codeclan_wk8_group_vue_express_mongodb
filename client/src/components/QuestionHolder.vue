1e07f4ca34534afdbcf94e2346ee210aea179b90
Nathan <nathankirley@gmail.com>
2020-05-25 11:16:11 +0100

    can access incorrect answers

--- client/src/components/QuestionHolder.vue
+++ client/src/components/QuestionHolder.vue
@@ -3,7 +3,12 @@
       <p>Question:</p>
       <ul v-for="(question, index) in questions" :question="question" :key="index">
           <li>{{question.question}}</li>
+          <p>Answer:</p>
           <li> {{question.correct_answer}} </li>
+          <p>Incorrect answers: </p>
+          <ul v-for="(qstn, index) in question.incorrect_answers" :key="index">
+              <li>{{qstn}}</li>
+          </ul>
       </ul>
   </div>
 </template>
