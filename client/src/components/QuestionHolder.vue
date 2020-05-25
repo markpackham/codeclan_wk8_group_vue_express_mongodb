1821cdea301469cac3453fb53f55605f585ebb70
Nathan <nathankirley@gmail.com>
2020-05-25 12:05:02 +0100

    can view a single question

--- client/src/components/QuestionHolder.vue
+++ client/src/components/QuestionHolder.vue
@@ -1,23 +1,34 @@
 <template>
-  <div>
+  <div class="country-quiz">
       <ul>
-      <question-detail v-for="(question, index) in questions" :question="question" :key="index"></question-detail>
+      <question-detail v-for="(question, index) in questions" :key="index"></question-detail>
       </ul>
+      <!-- :question="question" -->
+      <p v-on:click="handleClick">Next question</p>
+      <p>{{aQuestion}}</p>
   </div>
 </template>
 
 <script>
-import QuestionDetail from './QuestionDetail.vue'
+import { eventBus } from '@/main.js';
+import QuestionDetail from './QuestionDetail.vue';
+
 export default {
     name: 'question-holder',
     props: ['questions'],
     data(){
         return {
-            question: null
+            aQuestion: ""
         }
     },
     components: {
       "question-detail": QuestionDetail
+    },
+    methods: {
+        handleClick() {
+            let randomNum = Math.floor(Math.random() * this.questions.length + 1);
+            this.aQuestion = this.questions[randomNum].question;
+        }
     }
 }
 </script>
