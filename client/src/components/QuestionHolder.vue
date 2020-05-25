8c8278fbb7a0bf1298a3c05268a22cd603095a73
Nathan <nathankirley@gmail.com>
2020-05-25 14:23:53 +0100

    order answers alphabetically instead of randomizing

--- client/src/components/QuestionHolder.vue
+++ client/src/components/QuestionHolder.vue
@@ -6,10 +6,6 @@
       <button v-on:click="handleClick">Next question</button>
       <div v-if="aQuestion">
       <p>{{aQuestion}}</p>
-      <!-- <ul><li><button>{{correctAnswer}}</button></li></ul>
-      <ul v-for="(qstn, index) in incorrectAnswers" :key="index">
-              <li><button>{{qstn}}</button></li>
-        </ul> -->
       <ul v-for="(qstn, index) in allAnswers" :key="index">
               <li><button>{{qstn}}</button></li>
         </ul>
@@ -27,8 +23,6 @@ export default {
     data(){
         return {
             aQuestion: "",
-            // correctAnswer: "",
-            // incorrectAnswers: [],
             allAnswers: []
         }
     },
@@ -40,11 +34,11 @@ export default {
             let randomNum = Math.floor(Math.random() * this.questions.length + 1);
             this.aQuestion = this.questions[randomNum].question;
             // this.correctAnswer = this.questions[randomNum].correct_answer;
-            // this.incorrectAnswers = this.questions[randomNum].incorrect_answers;
             this.allAnswers.splice(0, 1, this.questions[randomNum].correct_answer)
             this.allAnswers.splice(1, 1, this.questions[randomNum].incorrect_answers[0])
             this.allAnswers.splice(2, 1, this.questions[randomNum].incorrect_answers[1])
             this.allAnswers.splice(3, 1, this.questions[randomNum].incorrect_answers[2])
+            this.allAnswers.sort()
         }
     }
 }
