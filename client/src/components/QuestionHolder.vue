2c8acee66c0a2f9562cdf216d093d40834918e70
Nathan <nathankirley@gmail.com>
2020-05-25 14:04:52 +0100

    all answers in one array

--- client/src/components/QuestionHolder.vue
+++ client/src/components/QuestionHolder.vue
@@ -4,11 +4,16 @@
       <question-detail v-for="(question, index) in questions" :key="index"></question-detail>
       </ul>
       <button v-on:click="handleClick">Next question</button>
+      <div v-if="aQuestion">
       <p>{{aQuestion}}</p>
-      <ul><li><button>{{correctAnswer}}</button></li></ul>
+      <!-- <ul><li><button>{{correctAnswer}}</button></li></ul>
       <ul v-for="(qstn, index) in incorrectAnswers" :key="index">
               <li><button>{{qstn}}</button></li>
+        </ul> -->
+      <ul v-for="(qstn, index) in allAnswers" :key="index">
+              <li><button>{{qstn}}</button></li>
         </ul>
+      </div>
   </div>
 </template>
 
@@ -22,8 +27,9 @@ export default {
     data(){
         return {
             aQuestion: "",
-            correctAnswer: "",
-            incorrectAnswers: []
+            // correctAnswer: "",
+            // incorrectAnswers: [],
+            allAnswers: []
         }
     },
     components: {
@@ -33,8 +39,12 @@ export default {
         handleClick() {
             let randomNum = Math.floor(Math.random() * this.questions.length + 1);
             this.aQuestion = this.questions[randomNum].question;
-            this.correctAnswer = this.questions[randomNum].correct_answer;
-            this.incorrectAnswers = this.questions[randomNum].incorrect_answers;
+            // this.correctAnswer = this.questions[randomNum].correct_answer;
+            // this.incorrectAnswers = this.questions[randomNum].incorrect_answers;
+            this.allAnswers.splice(0, 1, this.questions[randomNum].correct_answer)
+            this.allAnswers.splice(1, 1, this.questions[randomNum].incorrect_answers[0])
+            this.allAnswers.splice(2, 1, this.questions[randomNum].incorrect_answers[1])
+            this.allAnswers.splice(3, 1, this.questions[randomNum].incorrect_answers[2])
         }
     }
 }
