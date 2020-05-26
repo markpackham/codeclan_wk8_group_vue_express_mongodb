fe65ff05884658d024d22f38195379d8b5991338
mark <markpackham1@gmail.com>
2020-05-26 07:21:49 +0100

    remove never used SHOW

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -18,19 +18,6 @@ const createRouter = function (collection) {
       });
   });
 
-  // SHOW (Used for Testing only)
-  router.get("/:id", (req, res) => {
-    const id = req.params.id;
-    collection
-      .findOne({ _id: ObjectID(id) })
-      .then((doc) => res.json(doc))
-      .catch((err) => {
-        console.error(err);
-        res.status(500);
-        res.json({ status: 500, error: err });
-      });
-  });
-
   // CREATE
   router.post("/", (req, res) => {
     const newFact = req.body;
